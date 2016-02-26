523b69b5654c656c6de4ba9f4edb29fced5a0444
Alexander Khlybov <alekshlybov23@gmail.com>
2016-02-26 10:54:14 +0300

    Initial.

--- include/d-heap.h
+++ include/d-heap.h
@@ -1,5 +1,30 @@
 #ifndef __D_HEAP__
 #define __D_HEAP__
+#include <iostream>
+typedef int KeyType;
 
+class D_HEAP{
+private:
+	KeyType*    tree_;
+    size_t      sizeTree_;
+	size_t      size_;
+	size_t      d_;
 
+	KeyType minChild 	(size_t);
+	KeyType min 		(size_t, size_t);
+protected:
+	void swap 		(size_t, size_t);
+	void siftDown 	(size_t);
+	void siftUp 		(size_t);
+public:
+	D_HEAP 	(size_t, size_t);
+	D_HEAP 	(const KeyType*, size_t, size_t);
+	D_HEAP 	(const D_HEAP&);
+	~D_HEAP (void);
+
+	void insert 		(const KeyType&);
+	void deleteMinElem 	(void);
+	void deleteElem 	(const KeyType&);
+	void heapify 		(void);
+};
 #endif
--- src/d-heap.cpp
+++ src/d-heap.cpp
@@ -1,1 +1,55 @@
 #include "d-heap.h"
+
+KeyType D_HEAP::minChild(size_t)
+{
+    return KeyType();
+}
+
+KeyType D_HEAP::min(size_t, size_t)
+{
+    return KeyType();
+}
+
+void D_HEAP::swap(size_t, size_t)
+{
+}
+
+void D_HEAP::siftDown(size_t)
+{
+}
+
+void D_HEAP::siftUp(size_t)
+{
+}
+
+D_HEAP::D_HEAP(size_t, size_t)
+{
+}
+
+D_HEAP::D_HEAP(const KeyType *, size_t, size_t)
+{
+}
+
+D_HEAP::D_HEAP(const D_HEAP &)
+{
+}
+
+D_HEAP::~D_HEAP(void)
+{
+}
+
+void D_HEAP::insert(const KeyType &)
+{
+}
+
+void D_HEAP::deleteMinElem(void)
+{
+}
+
+void D_HEAP::deleteElem(const KeyType &)
+{
+}
+
+void D_HEAP::heapify(void)
+{
+}
