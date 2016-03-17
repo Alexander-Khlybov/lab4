d70f3fdf8a6178eed196fb640e7f50d6d6e286be
Alexander Khlybov <alekshlybov23@gmail.com>
2016-03-17 18:28:38 +0300

    Initial class structure.

---
+++ include/priority_queue.h
@@ -0,0 +1,52 @@
+#ifndef __PRIORITY_QUEUE__
+#define __PRIORITY_QUEUE__
+
+#ifndef __D_HEAP__
+#include "d-heap.h"
+#endif
+
+#ifndef _CSTDLIB_
+#include <cstdlib>
+#endif
+
+#ifndef __TVECTOR_H__
+#include "tvector.h"
+#endif
+
+#ifndef __EXCP_H__
+#include "excp.h"
+#endif
+
+#ifndef __TVECTOR_H__
+#include <iostream>
+using namespace std;
+typedef int KeyType;
+#endif
+
+class PRIORITY_QUEUE{
+
+	D_HEAP* heap_;
+public:
+    PRIORITY_QUEUE  (int d = 1);
+    PRIORITY_QUEUE  (const PRIORITY_QUEUE&);
+	~PRIORITY_QUEUE (void) { delete heap_;};
+
+    int     getSize (void) const;
+    int     getHeap     (void) const;
+
+    int     operator==  (const PRIORITY_QUEUE&)const;
+    int     operator!=  (const PRIORITY_QUEUE&)const;
+
+    int     isFull      (void) const;
+    int     isEmpty     (void) const;
+
+    void    pop         (void);
+    void    push        (const KeyType&);
+    KeyType back        (void) const;
+
+    friend ostream& operator<< (ostream& out, const PRIORITY_QUEUE& heap) {
+            out << heap;
+        return out;
+    }
+};
+#endif
---
+++ src/priority_queue.cpp
@@ -0,0 +1,52 @@
+#include "priority_queue.h"
+
+PRIORITY_QUEUE::PRIORITY_QUEUE(int d)
+{
+}
+
+PRIORITY_QUEUE::PRIORITY_QUEUE(const PRIORITY_QUEUE &)
+{
+}
+
+int PRIORITY_QUEUE::getSize(void) const
+{
+    return 0;
+}
+
+int PRIORITY_QUEUE::getHeap(void) const
+{
+    return 0;
+}
+
+int PRIORITY_QUEUE::operator==(const PRIORITY_QUEUE &) const
+{
+    return 0;
+}
+
+int PRIORITY_QUEUE::operator!=(const PRIORITY_QUEUE &) const
+{
+    return 0;
+}
+
+int PRIORITY_QUEUE::isFull(void) const
+{
+    return 0;
+}
+
+int PRIORITY_QUEUE::isEmpty(void) const
+{
+    return 0;
+}
+
+void PRIORITY_QUEUE::pop(void)
+{
+}
+
+void PRIORITY_QUEUE::push(const KeyType &)
+{
+}
+
+KeyType PRIORITY_QUEUE::back(void) const
+{
+    return KeyType();
+}
