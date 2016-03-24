304a32c1cad41c91a8c07208d60f69c34707173f
Alexander Khlybov <alekshlybov23@gmail.com>
2016-03-24 22:14:39 +0300

    Graph imp.

--- include/graph.h
+++ include/graph.h
@@ -1,12 +1,13 @@
 #ifndef __GRAPH_H__
 #define __GRAPH_H__
 #include <limits>
+#include <iostream>
 using namespace std;
 const size_t MAX_VERTICE_NUM = 100;
 
 class GRAPH{
 	double** dist_;
-	size_t numberОfVertices_;
+	size_t vertices_;
     const double _INFINITY_;
 public:
     GRAPH(size_t max_num_of_vert = 10);
@@ -15,7 +16,7 @@ public:
 
 	void    setDistance         (size_t, size_t, double);
 	double  getDistance         (size_t, size_t)const;
-    int     getNumOfVertices    (void)const { return numberОfVertices_; }
+    int     getNumOfVertices    (void)const { return vertices_; }
     double  getInf              (void)const { return _INFINITY_;}
 };
 
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,17 +1,61 @@
 #include "..\include\graph.h"
 
 GRAPH::GRAPH(size_t max_num_of_vert) : _INFINITY_(numeric_limits<double>::infinity()) {
+    if ((max_num_of_vert > MAX_VERTICE_NUM) || (max_num_of_vert == 0))
+        throw out_of_range("Out of range.");
+
+    vertices_ = max_num_of_vert;
+
+    dist_ = new double*[max_num_of_vert - 1];
+    if (dist_ == NULL)
+        throw bad_alloc();
+
+    for (size_t i = 0; i < max_num_of_vert - 1; i++) {
+        dist_[i] = new double[max_num_of_vert - i - 1];
+        if (dist_[i] == NULL)
+            throw bad_alloc();
+    }
+
+    for (size_t i = 0; i < max_num_of_vert - 1; i++) {
+        for (size_t j = i; j < max_num_of_vert - 1; j++)
+            dist_[i][j] = 0;
+    }
+
 }
 
 GRAPH::GRAPH(const GRAPH& graph) : _INFINITY_(numeric_limits<double>::infinity()) {
+    vertices_ = graph.vertices_;
+
+    dist_ = new double*[vertices_ - 1];
+    if (dist_ == NULL)
+        throw bad_alloc();
+
+    for (size_t i = 0; i < vertices_ - 1; i++) {
+        dist_[i] = new double[vertices_ - i - 1];
+        if (dist_[i] == NULL)
+            throw bad_alloc();
+    }
+
+    for (size_t i = 0; i < vertices_ - 1; i++) {
+        for (size_t j = i; j < vertices_ - 1; j++)
+            dist_[i][j] = graph.dist_[i][j];
+    }
 }
 
 GRAPH::~GRAPH(void){
+    for (size_t i = 0; i < vertices_ - 1; i++)
+        delete[]dist_[i];
+    delete[]dist_;
 }
 
 void GRAPH::setDistance(size_t first, size_t second, double dist){
+    if ((first >= vertices_) || (second >= vertices_))
+        throw out_of_range("Ouet of range.");
+    dist_[first][second] = dist;
 }
 
 double GRAPH::getDistance(size_t first, size_t second) const{
-    return 0;
+    if ((first >= vertices_) || (second >= vertices_))
+        throw out_of_range("Ouet of range.");
+    return dist_[first][second];
 }
