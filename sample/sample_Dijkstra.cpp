ac122eeea1e052e4414097307af190fad204131b
Alexander Khlybov <alekshlybov23@gmail.com>
2016-04-22 00:38:35 +0300

    Dijkstra-imp.

--- sample/sample_Dijkstra.cpp
+++ sample/sample_Dijkstra.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <set>
 #include <vector>
 #include <algorithm>
 #include "graph.h"
@@ -7,80 +6,78 @@
 
 int main(void) {
 
-    size_t numVert = 1;
-    std::cout << "Enter the number of vertices: ";
-    std::cin >> numVert;
+	size_t numVert = 1;
+	std::cout << "\nEnter the number of vertices: ";
+	std::cin >> numVert;
 
-//  *************|| FILLING OF GRAPH *************
-    GRAPH* graph;
-    try {
-        graph = new GRAPH(numVert);
-    }catch (exception& e) {
-        std::cout << e.what() << std::endl;
-        delete graph;
-        return 1;
-    }
+	//  *************|| FILLING OF GRAPH *************
+	GRAPH* graph;
+	try {
+		graph = new GRAPH(numVert);
+	}
+	catch (exception& e) {
+		std::cout << e.what() << std::endl;
+		delete graph;
+		return 1;
+	}
 
-    try {
-        graph->fillGraph();
-    }catch (exception& e) {
-        std::cout << e.what() << std::endl;
-        delete graph;
-        return 1;
-    }
+	try {
+		graph->fillGraph();
+	}
+	catch (exception& e) {
+		std::cout << e.what() << std::endl;
+		delete graph;
+		return 1;
+	}
 
-//  *************|| LABEL *************
-    size_t currentVertex = 0;
-    std::cout << "Enter the current vertex: ";
-    std::cin >> currentVertex;
-    if (currentVertex >= numVert) {
-        std::cout << "Vertex does not exist.\n";
-        delete graph;
-        return 1;
-    }
+	//  *************|| LABEL *************
+	size_t currentVertex = 0;
+	std::cout << "\nEnter the current vertex: ";
+	std::cin >> currentVertex;
+	if (currentVertex >= numVert) {
+		std::cout << "\nVertex does not exist.\n";
+		delete graph;
+		return 1;
+	}
 
-//  *************|| INITIAL VECTOR OF DISTANCES AND VISITED VERTICES *************
-    std::vector<DISTANCE> dist(numVert);
-    dist[currentVertex].vertex = currentVertex;
-    dist[currentVertex].distance = 0;
+	//  *************|| INITIAL VECTOR OF DISTANCES AND VISITED VERTICES *************
+	std::vector<DISTANCE> dist(numVert);
 
-//  *************|| INITIAL PRYORITY QUEUE *************
-    PRIORITY_QUEUE<DISTANCE> queue;
-    queue.push(DISTANCE(dist[currentVertex]));
+	dist[currentVertex].distance = 0;
+	dist[currentVertex].vertex = currentVertex;
 
-//  *************|| DIJKSTRA CYCLE *************
-    while(!queue.isEmpty()){
-        DISTANCE d = queue.back();
-        queue.pop();
-        if (d > dist[d.vertex]) continue;
-        std::set<DISTANCE> edges = graph->getSetOfEdges(d.vertex);
-        for(DISTANCE k : edges){
-            size_t tmp = k.vertex;
-            double len = k.distance;
-            if(dist[tmp].distance > dist[d.vertex].distance + len){
-                dist[tmp].vertex = d.vertex;
-                dist[tmp].distance = dist[d.vertex].distance + len;
-                queue.push(DISTANCE(tmp, dist[tmp].distance));
-            }
-        }
-    }
+	//  *************|| INITIAL PRYORITY QUEUE *************
+	PRIORITY_QUEUE<DISTANCE> queue;
+	queue.push(dist[currentVertex]);
 
-//  *************|| RESULT OUTPUT *************
-    std::cout << "Dijkstra result: \n -------------------------";
-    std::cout << "Current vertex: " << currentVertex << "\n ===========================";
-    std::cout << "Vertex:\t";
-    for (size_t i = 0; i < numVert; i++) {
-        if(dist[i].vertex != currentVertex)
-            std::cout << i << "\t";
-    }
+	//  *************|| DIJKSTRA CYCLE *************
+	while (!queue.isEmpty()) {
+		DISTANCE d = queue.back();
+		queue.pop();
+		if (d.distance > dist[d.vertex].distance) continue;
+		std::set<DISTANCE> a = graph->getSetOfEdges(d.vertex);
+		for_each(a.begin(), a.end(), [&] (DISTANCE k) {
+			size_t tmp = k.vertex;
+			double len = k.distance;
+			if (dist[tmp].distance > dist[d.vertex].distance + len) {
+				dist[tmp].vertex = d.vertex;
+				dist[tmp].distance = dist[d.vertex].distance + len;
+				queue.push(DISTANCE(tmp, dist[tmp].distance));
+			}
+		});			
+	}
 
-    std::cout << "\nDistance:\t";
-    for (size_t i = 0; i < numVert; i++) {
-        if (dist[i].vertex != currentVertex)
-            std::cout << dist[i].distance << "\t";
-    }
-    std::cout << "\n\n";
+	//  *************|| RESULT OUTPUT *************
+	graph->graphInfo();
 
-    delete graph;
-    return 0;
+	std::cout << "Dijkstra result: \n -------------------------";
+	std::cout << "Current vertex: " << currentVertex << "\n ===========================";
+	std::cout << "\nVertices:\t";
+	for (size_t i = 0; i < dist.size(); i++) std::cout << i << "\t";
+	std::cout << "\nDistances:\t";
+	for (auto x : dist) std::cout << x.distance << "\t";
+	std::cout << "\n\n";
+
+	delete graph;
+	return 0;
 }
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,90 +1,45 @@
 #include "..\include\graph.h"
 
-GRAPH::GRAPH(size_t max_num_of_vert){
-    if ((max_num_of_vert > MAX_VERTICE_NUM) || (max_num_of_vert == 0))
-        throw std::out_of_range("Out of range.");
-
-    vertices_ = max_num_of_vert;
-
-    dist_ = new double*[max_num_of_vert];
-    if (dist_ == NULL)
-        throw std::bad_alloc();
-
-    for (size_t i = 0; i < max_num_of_vert; i++) {
-        dist_[i] = new double[max_num_of_vert - i];
-        if (dist_[i] == NULL)
-            throw std::bad_alloc();
-    }
-
-    for (size_t i = 0; i < max_num_of_vert; i++) {
-        for (size_t j = i; j < max_num_of_vert; j++)
-            dist_[i][j] = 0;
-    }
-
-}
-
 GRAPH::GRAPH(const GRAPH& graph){
     vertices_ = graph.vertices_;
-
-    dist_ = new double*[vertices_];
-    if (dist_ == NULL)
-        throw std::bad_alloc();
-
-    for (size_t i = 0; i < vertices_; i++) {
-        dist_[i] = new double[vertices_ - i];
-        if (dist_[i] == NULL)
-            throw std::bad_alloc();
-    }
-
-    for (size_t i = 0; i < vertices_; i++) {
-        for (size_t j = i; j < vertices_; j++)
-            dist_[i][j] = graph.dist_[i][j];
-    }
-}
-
-GRAPH::~GRAPH(void){
-    for (size_t i = 0; i < vertices_; i++)
-        delete[]dist_[i];
-    delete[]dist_;
+	graph_ = graph.graph_;
 }
 
 void GRAPH::setDistance(size_t first, size_t second, double dist){
     if ((first >= vertices_) || (second >= vertices_))
         throw std::out_of_range("Out of range.");
-
-    if (first == second) {
-        dist_[first][second] = 0;
-        return;
-    }
-
-    if (first < second) {
-        dist_[first][second] = dist;
-        return;
-    }
-    dist_[second][first] = dist;
+	graph_.insert(pair<size_t, DISTANCE>(MIN(first, second), 
+					DISTANCE(MAX(first, second), dist)));
 }
 
 double GRAPH::getDistance(size_t first, size_t second) const{
     if ((first >= vertices_) || (second >= vertices_))
         throw std::out_of_range("Out of range.");
-    return (first == second) ? 0 : ((first < second) ? 
-        dist_[first][second] : dist_[second][first]);
+	double dist = 0;
+	for (auto x : graph_) {
+		if (x.first == first && x.second.vertex == second ||
+			x.first == second && x.second.vertex == first) {
+			dist = x.second.distance;
+			break;
+		}
+	}
+	return dist;
 }
 
 void GRAPH::fillGraph(void){
     while (true) {
         size_t first = 0;
-        std::cout << "Enter the first vertex: ";
+        std::cout << "\nEnter the first vertex: ";
         std::cin >> first;
         size_t second = 0;
-        std::cout << "Enter the second vertex: ";
+        std::cout << "\nEnter the second vertex: ";
         std::cin >> second;
         double distance = 0;
-        std::cout << "Enter the distance between the vertices: ";
+        std::cout << "\nEnter the distance between the vertices: ";
         std::cin >> distance;
         setDistance(first, second, distance);
         char s;
-        std::cout << "To complete the filling of the graph, type 'e': ";
+        std::cout << "\nTo complete the filling of the graph, type 'e': ";
         std::cin >> s;
         if (s == 'e')
             break;
@@ -94,17 +49,19 @@ void GRAPH::fillGraph(void){
 std::set<DISTANCE> GRAPH::getSetOfEdges(size_t currentVertex)const{
     if (currentVertex >= vertices_)
         throw std::out_of_range("Out of range.");
-
-    std::set<DISTANCE> result;
-    for (size_t i = 0; i < vertices_ - currentVertex; i++){
-        if (dist_[currentVertex][i] != 0)
-            result.insert(DISTANCE(i, dist_[currentVertex][i]));
-    }
-
-    for (size_t i = 1; i < vertices_ - currentVertex; i++){
-        if(dist_[i][currentVertex] != 0)
-            result.insert(DISTANCE(i, dist_[i][currentVertex]));
-    }
-
+	std::set<DISTANCE> result;
+	for (size_t i = 0; i < vertices_; i++) {
+		double dist = getDistance(i, currentVertex);
+		if (dist != 0) result.insert(DISTANCE(i, dist));
+	}
     return result;
+}
+
+void GRAPH::graphInfo(void)const{
+	std::cout << "\n=========== EDGES ===========\n\n";
+	for (auto x : graph_) {
+		std::cout << x.first << "<-->" << x.second.vertex 
+			<< "  ::  " << x.second.distance << "\t|||\t";
+	}
+	std::cout << "\n\n";
 }
